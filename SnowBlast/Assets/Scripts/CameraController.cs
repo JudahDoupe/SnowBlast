d445987c52786ecd599efc5a66af8e82623afea4
judahdoupe <judahdoupe@gmail.com>
2021-02-13 17:44:24 -0500

    Arena scene added and scrolling camera

--- SnowBlast/Assets/Scripts/CameraController.cs
+++ SnowBlast/Assets/Scripts/CameraController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,18 +7,28 @@ public class CameraController : MonoBehaviour
 {
     public GameObject Player;
     public float Speed;
-    public float Distance;
+    public float ZoomSpeed;
 
-    // Start is called before the first frame update
+    public float MinDistance;
+    public float MaxDistance;
+    private float distance;
+
+    public float MinAngle;
+    public float MaxAngle;
+    private float angle;
+
     void Start()
     {
-        
+        distance = (MaxDistance - MinDistance) / 2 + MinDistance;
     }
 
-    // Update is called once per frame
-    void LateUpdate()
+    void FixedUpdate ()
     {
-        var targetPosition = Player.transform.position + new Vector3(0,1,-1) * Distance;
+        distance = Mathf.Clamp(distance - Input.mouseScrollDelta.y * ZoomSpeed, MinDistance, MaxDistance);
+        angle = Mathf.Lerp(MinAngle, MaxAngle, (distance - MinDistance) / (MaxDistance - MinDistance));
+
+        var targetPosition = Player.transform.position + new Vector3(0,1,-1) * distance;
         transform.position = Vector3.Lerp(transform.position, targetPosition, Time.deltaTime * Speed);
+        transform.eulerAngles = Vector3.Lerp(transform.eulerAngles, new Vector3(angle, 0, 0), Time.deltaTime * ZoomSpeed);
     }
 }
